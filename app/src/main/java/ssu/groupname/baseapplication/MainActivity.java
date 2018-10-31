b810a6e85e92768365d8bf439cdc3f12ffe7199f
wgoodson <goodsonw@sonoma.edu>
2018-10-30 19:38:19 -0700

    OpenCV integration is a success!

--- app/src/main/java/ssu/groupname/baseapplication/MainActivity.java
+++ app/src/main/java/ssu/groupname/baseapplication/MainActivity.java
@@ -3,11 +3,19 @@ package ssu.groupname.baseapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.opencv.core.Mat;
+import org.opencv.core.Core;
+import org.opencv.android.Utils;
+import org.opencv.imgproc.Imgproc;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.loadLibrary("opencv_java3");
     }
 }

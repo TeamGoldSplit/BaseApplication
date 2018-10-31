5aab7205c238c17cf6b4e3759f3c13a7678c3a49
pseuudonym <lucasbv14@gmail.com>
2018-10-30 20:13:40 -0700

    please work

--- app/src/main/java/ssu/groupname/baseapplication/FinalActivity.java
+++ app/src/main/java/ssu/groupname/baseapplication/FinalActivity.java
@@ -1,13 +1,28 @@
 package ssu.groupname.baseapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FinalActivity extends AppCompatActivity {
+
+    private Button topButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final);
+
+        //backButton
+        topButton = findViewById(R.id.camera_button);
+        topButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backIntent = new Intent(FinalActivity.this, MainActivity.class);
+                startActivity(backIntent);
+            }
+        });
     }
 }
--- app/src/main/java/ssu/groupname/baseapplication/OperationsActivity.java
+++ app/src/main/java/ssu/groupname/baseapplication/OperationsActivity.java
@@ -1,13 +1,48 @@
 package ssu.groupname.baseapplication;
 
+import android.content.Intent;
+import android.media.VolumeShaper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class OperationsActivity extends AppCompatActivity {
+
+    private Button backButton;
+    private Button computeButton;
+    private String origin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_operations);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        origin = intent.getStringExtra("origin");
+
+        //backButton
+        backButton = findViewById(R.id.camera_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backIntent;
+                if (origin == "camera")
+                    backIntent = new Intent(OperationsActivity.this, CameraActivity.class);
+                else //yeah this is kinda sloppy but I don't wanna write the exception catching rn
+                    backIntent = new Intent(OperationsActivity.this, FileActivity.class);
+                startActivity(backIntent);
+            }
+        });
+
+        //opsButton
+        computeButton = findViewById(R.id.compute_button);
+        computeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent computeIntent = new Intent(OperationsActivity.this, FinalActivity.class);
+                startActivity(computeIntent);
+            }
+        });
     }
 }
